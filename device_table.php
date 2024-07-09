51779d7d2153e834ff67a4272acff331ef355ac4
Wong Kai Quan <kaiquanwong@gmail.com>
2024-07-09 20:39:09 +0800

    added placeholder data for table

--- device_table.php
+++ device_table.php
@@ -12,5 +12,35 @@
         </tr>
     </thead>
     <tbody>
+        <tr>
+            <td>1</td>
+            <td>1234</td>
+            <td>5678</td>
+            <td>91011</td>
+            <td>80%</td>
+            <td>90%</td>
+            <td>None</td>
+            <td>2021-09-01 10:30:00</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>4321</td>
+            <td>8765</td>
+            <td>11109</td>
+            <td>70%</td>
+            <td>80%</td>
+            <td>Low Battery</td>
+            <td>2021-09-02 09:45:00</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>9876</td>
+            <td>5432</td>
+            <td>90876</td>
+            <td>90%</td>
+            <td>95%</td>
+            <td>None</td>
+            <td>2021-09-03 14:20:00</td>
+        </tr>
     </tbody>
 </table>
