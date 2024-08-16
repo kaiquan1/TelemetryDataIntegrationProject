0aa7006fe2f2ab26ce9cb447af7704fcd6a4e566
Wong Kai Quan <kaiquanwong@gmail.com>
2024-08-16 22:41:09 +0800

    changed View_ArchivedInformations to View_FetchTableData

--- fetch_table_data.php
+++ fetch_table_data.php
@@ -18,42 +18,42 @@ OUTER APPLY (
     SELECT TOP 1 
         a.INF_Value,
         a.INF_Date
-    FROM [ScadaNetDb].[dbo].[View_ArchivedInformations] a
+    FROM [ScadaNetDb].[dbo].[View_FetchTableData] a
     WHERE a.STA_SiteNumber = s.STA_SiteNumber AND a.INF_NumberInStation = 1
     ORDER BY a.Archive_ID DESC
 ) a1
 OUTER APPLY (
     SELECT TOP 1 
         a.INF_Value
-    FROM [ScadaNetDb].[dbo].[View_ArchivedInformations] a
+    FROM [ScadaNetDb].[dbo].[View_FetchTableData] a
     WHERE a.STA_SiteNumber = s.STA_SiteNumber AND a.INF_NumberInStation = 2
     ORDER BY a.Archive_ID DESC
 ) a2
 OUTER APPLY (
     SELECT TOP 1 
         a.INF_Value
-    FROM [ScadaNetDb].[dbo].[View_ArchivedInformations] a
+    FROM [ScadaNetDb].[dbo].[View_FetchTableData] a
     WHERE a.STA_SiteNumber = s.STA_SiteNumber AND a.INF_NumberInStation = 3
     ORDER BY a.Archive_ID DESC
 ) a3
 OUTER APPLY (
     SELECT TOP 1 
         a.INF_Value
-    FROM [ScadaNetDb].[dbo].[View_ArchivedInformations] a
+    FROM [ScadaNetDb].[dbo].[View_FetchTableData] a
     WHERE a.STA_SiteNumber = s.STA_SiteNumber AND a.INF_NumberInStation = 17
     ORDER BY a.Archive_ID DESC
 ) a17
 OUTER APPLY (
     SELECT TOP 1 
         a.INF_Value
-    FROM [ScadaNetDb].[dbo].[View_ArchivedInformations] a
+    FROM [ScadaNetDb].[dbo].[View_FetchTableData] a
     WHERE a.STA_SiteNumber = s.STA_SiteNumber AND a.INF_NumberInStation = 49
     ORDER BY a.Archive_ID DESC
 ) a49
 OUTER APPLY (
     SELECT TOP 1 
         a.INF_Value
-    FROM [ScadaNetDb].[dbo].[View_ArchivedInformations] a
+    FROM [ScadaNetDb].[dbo].[View_FetchTableData] a
     WHERE a.STA_SiteNumber = s.STA_SiteNumber AND a.INF_NumberInStation = 44
     ORDER BY a.Archive_ID DESC
 ) a44
